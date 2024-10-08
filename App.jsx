71317b0faede73d329f59978231dcfcee31cc252
Luqmanulicious <d20221101871@siswa.upsi.edu.my>
2024-10-08 23:08:17 +0800

    Second Commit

--- App.jsx
+++ App.jsx
@@ -3,20 +3,34 @@ import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className = "text-xl">Assalamualaikum Dunia</Text>
-      <Text>Hike Mate</Text>
-      <Image className= "w-52 h-96" source={require('./hiking.jpeg')} />
-      <StatusBar style="auto" />
-      <Button
-  onPress={() => {
-    console.log('You tapped the button!');
-  }}
-  title="Press Me"
-/>
-    </View>
-    
+    <View className = "flex-1 justify-start items-center p-32"> 
+    <View className = "w-96 h-16 bg-emerald-400 -top-10 rounded-lg">
+      <View className = "flex-row">
+        <Image className = "w-10 h-10 top-3 left-3 rounded-full"  source={require("./assets/hiking.jpeg")}></Image>
+        <Text className = "text-lg font-semibold text-left top-4 left-5 italic" >HikeMate </Text>
+      </View>
+     </View>
+
+     <View className="w-screen">
+      <Image 
+        className = "w-full h-52" 
+        source={require("./assets/HikeMate_Exp.png")}
+        resizeMode="contain()"
+        ></Image>
+     </View>
+
+     <View className= "right-36 top-3">
+      <Text className= "text-xl font-semibold text-left">Activity</Text>
+     </View>
+
+     <View className="flex-row top-5 rounded-md">
+      <View className = "w-16 h-16 bg-black mr-2"></View>
+      <View className = "w-16 h-16 bg-black mr-2"></View>
+      <View className = "w-16 h-16 bg-black mr-2"></View>
+      <View className = "w-16 h-16 bg-black mr-2"></View>
+
+     </View>
+     </View>
+
   );
 }
-
-
