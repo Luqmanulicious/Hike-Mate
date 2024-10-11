835647b71634bba091ae42eb9d5ec88c81d9bc86
Luqmanulicious <d20221101871@siswa.upsi.edu.my>
2024-10-11 22:47:25 +0800

    recommendation

--- Screens/Recommendation.jsx
+++ Screens/Recommendation.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
-import { Image, ImageBackground, Text, View } from 'react-native'
+import { Image, ImageBackground, ScrollView, Text, View } from 'react-native'
 import ImageSlider from 'react-native-image-slider'
 import Slideshow from 'react-native-image-slider-show'
 
 export class Recommendation extends Component {
   render() {
     return (
-      <View>
+      <View className="left-12">
+        <ScrollView className="-mx-16 pl-2" >
         <View className= "h-16 w-full rounded-md bg-emerald-400"> 
             <Image className="w-10 h-10 top-3 left-3 rounded-full" source={require("../assets/hiking.jpeg")}></Image>
             <Text className="left-16 -top-5 text-lg font-serif font-semibold">Hike Mate</Text>
@@ -21,9 +22,9 @@ export class Recommendation extends Component {
             </ImageBackground> 
         </View>
 
-        <View className="">
-            <Text className="text-2xl top-8 font-bold">Near Me</Text>
-            <View className="justify-center items-center">
+        <View>
+            <Text className="text-2xl top-8 font-bold left-3">Near Me</Text>
+            <View className="justify-center items-center top-8">
                 {/* <Image className="top-10 w-96 h-56" source={require("../assets/Stong_1.jpg")}></Image> */}
                 <Slideshow
       dataSource={[
@@ -34,16 +35,31 @@ export class Recommendation extends Component {
             </View>
         </View>
 
-        <View className="top-10 left-1">
+        <View className="top-10 left-3">
             <Text className="text-xl">Gunung Stong,Kelantan</Text>
-            <Text className="text-base">Dabong, Kelantan</Text>
-            <Text className="text-base">4.4 . Hard . 8.5km . Est 1h 50m </Text>
-        </View>
-
-        <View className="top-14 left-1">
-            <Text className="text-2xl font-bold">Popular</Text>
+            <Text className="text-base">Dabong, Kelantan</Text>    
+            <Text className="text-base">⭐ 4.4 . Hard . 8.5km . Est 1h 50m </Text>
             
         </View>
+
+        <View className="top-14 left-3">
+            <Text className="text-2xl font-bold">Popular</Text>
+            <Image className="top-2 left-14 w-72 h-48 rounded-md" source={require("../assets/Kinabalu 2.jpeg")}></Image>
+            <View className="left-2 top-2">
+                <Text className="top-1 text-lg">Mount Kinabalu</Text>
+                <Text className="text-base">Ranau, Sabah</Text>
+                <Text className="text-base">⭐ 4.9 (2138) . Hard . 22.4km . Est 49h </Text>
+            </View>
+        </View>
+
+        <View className="top-24">
+            <Image className="left-14 w-72 h-48 rounded-md" source={require("../assets/Penang HH.jpeg")}></Image>
+            <Text className="top-1 text-lg">Penang Hill Heritage</Text>
+            <Text className="text-base">Ranau, Sabah</Text>
+            <Text className="text-base">⭐ 4.9 (2138) . Hard . 8.5km . Est 49h</Text>    
+        </View>
+        
+        </ScrollView>
       </View>
     )
 }
